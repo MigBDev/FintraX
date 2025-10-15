1c018b3dedf25545e6bfd86349351287a23add7f
Miguel Becerra <contact.lmbecerra@gmail.com>
2025-10-15 00:02:51 -0500

    🚀 Cambiar BrowserRouter por HashRouter para que Fintrax funcione en GitHub Pages

--- fintrax-homepage/src/App.jsx
+++ fintrax-homepage/src/App.jsx
@@ -1,4 +1,5 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
+
 import Home from "./pages/Home";
 import LoginPage from "./pages/loginPage";
 import RegisterPage from "./pages/RegisterPage";
